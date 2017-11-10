cc1720ce1b0f6b72950653c8e5009b45351b60b9
Bartosz Lewandowski <ognykb@gmail.com>
2017-11-10 20:55:59 +0100

    add notification by mail

--- LoginBlocker.php
+++ LoginBlocker.php
@@ -7,24 +7,52 @@ use yii\base\Component;
 class LoginBlocker extends Component
 {
     /**
-     * Time to block in seconds (default 5 min)
-     *
+     * Time to block/ban user in seconds (default 5 min)
      * @var integer
      */
     private $time = 300;
     
-    
     /**
      * Number of wrong attempts (default 3 times)
-     *
      * @var integer
      */
     private $wrong_login_number = 3;
     
+    /**
+     * List of mails
+     * @var array
+     */
+    private $mails = [];
+    
+    /**
+     * Mail subject
+     * Mark {ip} will be replace by user IP
+     * @var string
+     */
+    private $mail_subject = 'LoginBlocker - This IP "{ip}" tried to login too many times.';
+    
+    /**
+     * Mail content
+     * Mark {ip} will be replace by user IP
+     * Mark {date} will be replace by date
+     * @var string
+     */
+    private $mail_content = '<b>LoginBlocker</b><br/>User IP: {ip}<br/>Date: {date}';
+    
+    /**
+     * Sender name
+     * @var string
+     */
+    private $mail_sender_name = 'LoginBlocker';
+    
+    /**
+     * Sender mail
+     * @var string
+     */
+    private $mail_sender_mail = 'no-reply@mail.com';
     
     /**
      * Implement parameters
-     *
      * @param array $params
      */
     public function __construct($params = [])
@@ -35,9 +63,23 @@ class LoginBlocker extends Component
         if (isset($params['wrong_login_number'])) {
             $this->wrong_login_number = $params['wrong_login_number'];
         }
+        if (isset($params['mail']['mails'])) {
+            $this->mails = $params['mail']['mails'];
+        }
+        if (isset($params['mail']['subject'])) {
+            $this->mail_subject = $params['mail']['subject'];
+        }
+        if (isset($params['mail']['content'])) {
+            $this->mail_content = $params['mail']['content'];
+        }
+        if (isset($params['mail']['sender']['name'])) {
+            $this->mail_sender_name = $params['mail']['sender']['name'];
+        }
+        if (isset($params['mail']['sender']['mail'])) {
+            $this->mail_sender_mail = $params['mail']['sender']['mail'];
+        }
     }
     
-
     /**
      * Block IP if wrong login or password
      * 
@@ -48,7 +90,7 @@ class LoginBlocker extends Component
         $blocker = Yii::$app->cache->get('login_blocker_' . $this->getIp());
         
         if ($blocker === false) {
-            $blocker = [1, time()];
+            $blocker = [1, time(), 0];
         } else { 
             $blocker[0]++;
             
@@ -62,7 +104,6 @@ class LoginBlocker extends Component
         return true;
     }
     
-    
     /**
      * Check if user can login
      * 
@@ -74,13 +115,22 @@ class LoginBlocker extends Component
         
         if ($blocker) {
             if (time() < $blocker[1]) {
+                // Send mails
+                if ($blocker[2] == 0 && count($this->mails) > 0) {
+                    $time = $blocker[1] - time();
+                    $blocker[2] = 1;
+                    
+                    Yii::$app->cache->set('login_blocker_' . $this->getIp(), $blocker, $time);
+                    
+                    $this->sendMails();
+                }
+                
                 return false;
             }
         }
 
         return true;
     }
-    
     
     /**
      * Get user IP
@@ -92,5 +142,25 @@ class LoginBlocker extends Component
         $ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
         
         return $ip;
+    }
+    
+    /**
+     * Send mails
+     */
+    private function sendMails()
+    {
+        $subject = str_replace('{ip}', $this->getIp(), $this->mail_subject);
+        $content = str_replace('{ip}', $this->getIp(), $this->mail_content);
+        $content = str_replace('{date}', date('Y-m-d H:i:s'), $content);
+        
+        foreach($this->mails as $mail) {
+            \Yii::$app->mailer->compose()
+                ->setHtmlBody($content)
+                ->setTextBody(strip_tags($content))
+                ->setFrom([$this->mail_sender_mail => $this->mail_sender_name])
+                ->setTo($mail)
+                ->setSubject($subject)
+                ->send();
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# yii2-login-blocker (Not ready yet)
+# yii2-login-blocker
 
 [![Latest Stable Version](https://poser.pugx.org/ognyk/yii2-login-blocker/v/stable)](https://packagist.org/packages/ognyk/yii2-login-blocker)
 [![Total Downloads](https://poser.pugx.org/ognyk/yii2-login-blocker/downloads)](https://packagist.org/packages/ognyk/yii2-login-blocker)
 [![License](https://poser.pugx.org/ognyk/yii2-login-blocker/license)](https://packagist.org/packages/ognyk/yii2-login-blocker)
 
-Blocked login for few minutes after 3 wrong login times.
+Block/ban login for few minutes after 3 wrong login times.
 
 ## Installation
 
@@ -32,9 +32,7 @@ like this:
     ```php
     'components' => [
         'loginBlocker' => [
-            'class' => '\ognyk\loginblocker\LoginBlocker',
-            'time' => 300,              //  optional
-            'wrong_login_number' => 3,  //  optional
+            'class' => '\ognyk\loginblocker\LoginBlocker'
         ]
     ]
     ```
@@ -45,7 +43,7 @@ like this:
     /* Check if user can login */
     \Yii::$app->loginBlocker->check();
     
-    /* Increment when wrong login or password */
+    /* Increment counter when wrong login or password */
     \Yii::$app->loginBlocker->block();
     ```
 3. Use `loginBlocker`:
@@ -63,3 +61,43 @@ like this:
         return ['error' => 'wrong_credentials'];
     }
     ```
+
+## Advance config
+
+1. More parameters:
+
+    ```php
+    'components' => [
+        'loginBlocker' => [
+            'class' => '\ognyk\loginblocker\LoginBlocker',
+            'time' => 300,              // Time to block/ban user in seconds (default 300 sec)
+            'wrong_login_number' => 3,  // Number of wrong attempts (default 3 times)
+        ]
+    ]
+    ```   
+    
+2. Notification of block/ban by e-mail:
+
+To use notification by e-mail configure \Yii::$app->mailer.
+All parameters without `mails` are optional.
+
+    ```php
+    'components' => [
+        'loginBlocker' => [
+            'class' => '\ognyk\loginblocker\LoginBlocker',
+            'mail' => [
+                'subject' => 'New subject with user IP {ip}',
+                'content' => 'User IP {ip}<br>Date: {date}',
+                'sender' => [
+                    'name' => 'Cezar II',
+                    'mail' => 'mail@mail.com',
+                ],
+                'mails' => [
+                    'admin1@mail.com',
+                    'admin2@mail.com',
+                    'admin3@mail.com',
+                ],
+            ]
+        ]
+    ]
+    ```  
